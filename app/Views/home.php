7628b21600509921feb80e5f06bedb26ff92cf59
jon <zhenagajo234@gmail.com>
2023-12-12 01:23:20 +0700

    style: add placeholder page first for login, register, and landing page

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,2 +1,11 @@
-<h2> Home Page <h2>
-<p> This is the Home Page <p>
+    <div class="container mx-auto px-6 py-16 text-center">
+        <div class="mx-auto max-w-lg">
+        <h1 class="text-3xl font-bold text-white-800  lg:text-4xl">Helping you achieve the lifestyle you wanted!</h1>
+        <p class="mt-6 text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero similique obcaecati illum mollitia.</p>
+        <button class="mt-6 rounded-lg bg-blue-600 px-6 py-2.5 text-center text-sm font-medium capitalize leading-5 text-white hover:bg-blue-500 focus:outline-none lg:mx-0 lg:w-auto">Start 14-Day free trial</button>
+        </div>
+
+        <div class="mt-10 flex justify-center">
+        <img class="h-96 w-full rounded-xl object-cover lg:w-4/5 my-[50px]" src="https://images.unsplash.com/photo-1502224562085-639556652f33?q=80" />
+        </div>
+    </div>
