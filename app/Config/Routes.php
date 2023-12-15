853510c4531bbff31fe27eeb6464f78a540b7694
jon <zhenagajo234@gmail.com>
2023-12-15 11:13:43 +0700

    feat: add routing for handling get request from the other website

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,8 +10,9 @@ $routes->setDefaultController('Users');
 
 
 $routes->get('/', 'Users::index');
-$routes->match(['get','post'],'/register','Users::register');
-$routes->match(['get','post'],'/login','Users::index');
-$routes->match(['post'],'/users','Profile::updateUser');
-$routes->get('/profile','Profile::index');
-$routes->get('/home','Home::index');
+$routes->match(['get', 'post'], '/register', 'Users::register');
+$routes->match(['get', 'post'], '/login', 'Users::index');
+$routes->match(['post'], '/users', 'Profile::updateUser');
+$routes->get('/profile', 'Profile::index');
+$routes->get('/home', 'Home::index');
+$routes->get('/calories-converter', 'Calorie::index');
