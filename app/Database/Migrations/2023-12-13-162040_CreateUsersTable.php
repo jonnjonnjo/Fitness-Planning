30a2427bb5bb94aa00b00d2b246e4c199feb7283
jon <zhenagajo234@gmail.com>
2023-12-17 10:16:11 +0700

    refactor: change the port to 9999

--- app/Database/Migrations/2023-12-13-162040_CreateUsersTable.php
+++ app/Database/Migrations/2023-12-13-162040_CreateUsersTable.php
@@ -1,42 +1,46 @@
 <?php
 
 namespace App\Database\Migrations;
+
 use CodeIgniter\Database\RawSql;
 use CodeIgniter\Database\Migration;
 
 class CreateUsersTable extends Migration
 {
+ 
     public function up()
     {
+        $forge = \Config\Database::forge();
+        $forge->createDatabase('fitness-plan-',true);
         $this->forge->addField([
-            "id" =>[
-                "type" =>"INT",
-                "constraint"=>11,
-                "unsigned"=>true,
-                "notNull"=>true,
-                "auto_increment"=>true,
+            "id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "unsigned" => true,
+                "notNull" => true,
+                "auto_increment" => true,
             ],
-            "firstname"=>[
-                "type"=>"VARCHAR",
-                "constraint"=>255,
-                "notNull"=>true,
+            "firstname" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+                "notNull" => true,
             ],
-            "lastname"=>[
-                "type"=>"VARCHAR",
-                "constraint"=>255,
-                "notNull"=>true,
+            "lastname" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+                "notNull" => true,
             ],
-            "email"=>[
-                "type"=>"VARCHAR",
-                "constraint"=>255,
-                "notNull"=>true,
+            "email" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+                "notNull" => true,
             ],
-            "password"=>[
-                "type"=>"VARCHAR",
-                "constraint"=>255,
-                "notNull"=>true,
+            "password" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+                "notNull" => true,
             ],
-            "created_at"=>[
+            "created_at" => [
                 'type' => 'TIMESTAMP',
                 'null' => false,
                 'default' => new RawSql('CURRENT_TIMESTAMP'),
@@ -44,20 +48,20 @@ class CreateUsersTable extends Migration
             'updated_at' => [
                 'type' => 'TIMESTAMP',
                 'null' => true,
-                'default' => null,  
+                'default' => null,
                 'on_update' => new RawSql('CURRENT_TIMESTAMP'),
             ],
-            'height' =>[
-                'type'=>'INT',
-                'default'=>null,
+            'height' => [
+                'type' => 'INT',
+                'default' => null,
             ],
             'weight' => [
-                'type'=>'INT',
-                'default'=>null,
+                'type' => 'INT',
+                'default' => null,
             ],
             'age' => [
-                'type'=>'INT',
-                'default'=>null,
+                'type' => 'INT',
+                'default' => null,
             ]
         ]);
 
@@ -66,7 +70,7 @@ class CreateUsersTable extends Migration
         // age INT,
 
         $this->forge->addPrimaryKey("id");
-        $this->forge->createTable('users');
+        $this->forge->createTable('users', true);
     }
 
     public function down()
